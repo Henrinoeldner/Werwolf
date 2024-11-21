09ee3f50d724876a94e428d13c33973b84eeb5d2
noeldnhe <henri.noeldner@frg.wwschool.de>
2024-11-21 15:07:25 +0100

    Totengreber hinzugefuegt Character classe nun abstract

---
+++ src/Totengreaber.java
@@ -0,0 +1,11 @@
+public class Totengreaber extends Character {
+    public Totengreaber(){
+        Leben=1;
+        Classe="Werwolf";
+        Team=0;
+        Reinfolge=16;
+    }
+    public void Feahikeit(){
+
+    }
+}

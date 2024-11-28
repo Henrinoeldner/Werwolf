39ead3c2c83bcb973572bbbe3d3798577b9e221e
noeldnhe <henri.noeldner@frg.wwschool.de>
2024-11-28 15:48:20 +0100

    IN erzehler wird nun die Liste mit Chakter gefuellt

--- src/Erzaehler.java
+++ src/Erzaehler.java
@@ -1,14 +1,28 @@
+import java.util.List;
 import java.util.Random;
 
 public class Erzaehler {
      public Character[] spielendecharactere = new Character[5];
-     public Character[] moeglichecharacters=
+     List<Character> moeglicheWerwolfcharacter;
+
+     public Erzaehler(){
+          moeglicheWerwolfcharacter.add(new Werwolf());
+          moeglicheWerwolfcharacter.add(new BlinderWerwolf());
+          moeglicheWerwolfcharacter.add(new Weißerwerwolf());
+          moeglicheWerwolfcharacter.add(new Werwolfseher());
+     }
      public void test(){
-     for(int i=0;i<spielendecharactere.length;i++){
-         spielendecharactere[i]=moeglichecharacters[(int)(Math.random()* moeglichecharacters.length)];
+          //Sorgt fuer ein verheltnis zwischen Werwoelfen und Dorfbewohneren(ein werwolf fuer alle drei dorfbeohner)
+          int anzahlwerwoelfe=(spielendecharactere.length-1)/3;
 
+          for(int i=0;i<anzahlwerwoelfe;i++){
+               int speicher=(int)(Math.random()* moeglicheWerwolfcharacter.size());
+               spielendecharactere[i]=moeglicheWerwolfcharacter.get(speicher);
+
+               //wenn die gewaelte charakter kein einfacher Werwolf ist wird er aus der Liste der moeglichen Charakteren entfernt.
+               if (speicher!=0) {
+                    moeglicheWerwolfcharacter.remove(speicher);
+               }
+          }
      }
-
-     }
-
 }
